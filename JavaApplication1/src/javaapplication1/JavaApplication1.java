4c280c42da9e1b55a1027020fbe2004cf55369e9
test <>
2019-01-27 19:46:48 -0500

    First commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author stest
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        
+        int system = 6;
+        //int i =sc.nextInt();
+
+        //System.out.println(system);
+        
+        
+    
+    }
+}
