59fa590182b17a2e98bc3432304f4daa30a7ab48
VladBona <vlad.bona@gmail.com>
2019-02-06 17:48:05 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -2,25 +2,26 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Vladyslav Banasko 
+991528801
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * 
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack -11, Queen =12, King = 13) A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers. This code is to be used in ICE1.
+ * When you create your own branch, add your name as a modifier.
+ *
  * @author
  */
 public class Card {
-    
-    private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
+
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
     /**
      * @return the suit
      */
@@ -48,6 +49,26 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-    
+
+    @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit + ", value=" + value + '}';
+    }
+
+    public boolean equals(Card card) {
+        boolean b = false;
+        if (this.suit.equals(card.suit) && (this.value == card.value)) {
+            b = true;
+        } else {
+            b = false;
+        } 
+        return b;
+    }
+    //To change body of generated methods, choose Tools | Templates.
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,31 +2,61 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Vladyslav Banasko 
+991528801
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
+ * @author
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+        for (int i = 0; i < magicHand.length; i++) {
+
             Card c = new Card();
+            c.setValue((int) ((Math.random() * 12) + 1));
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your suit :Hearts,Diamonds,Spades, Clubs ");
+        String cardSuit = sc.nextLine();
+        System.out.println("Input your card , note that Ace = 1, Jack -11, Queen =12, King = 13");
+        int cardNum = sc.nextInt();
+
+        Card userCard = new Card();
+        userCard.setSuit(cardSuit);
+        userCard.setValue(cardNum);
+
+        for (Card card : magicHand) {
+            System.out.println(card.toString());
+        }
+
+        for (int i = 0; i < magicHand.length; i++) {
+            if (userCard.equals(magicHand[i])) {
+                System.out.println("equals");
+                break;
+            } else {
+                System.out.println("not equal");
+                break;
+            }
+
+            //insert code to ask the user for Card value and suit, create their card
+            // and search magicHand here
+            //Then report the result here
+        }
+
     }
-    
 }
