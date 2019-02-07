04a6a17eb463e22c2cd8e6bda9ded9ae1d7d067d
BuildTools <unconfigured@null.spigotmc.org>
2019-02-07 13:45:02 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,28 +5,65 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
+ * @author Brandon McCulloch 991529206
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        int selectedCard;
+        int selectedSuit;
+        int matchCount = 0;
+
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(randomNumber(13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[randomNumber(3)]);
+            //add the card to the magicHand array
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Please enter a card value [Ace = 1, Jack = 11, Queen = 12, King = 13]: ");
+        selectedCard = input.nextInt();
+        System.out.println("Please select a suit: ");
+        System.out.println("[0] Hearts");
+        System.out.println("[1] Diamonds");
+        System.out.println("[1] Spades");
+        System.out.println("[1] Clubs");
+        selectedSuit = input.nextInt();
+
         // and search magicHand here
+        for(Card card:magicHand) {
+            if(card.getSuit().equals(Card.SUITS[selectedSuit]) && card.getValue() == selectedCard) {
+                matchCount++;
+            }
+        }
         //Then report the result here
+        if (matchCount > 0) {
+            System.out.println("There was a match!");
+        } else {
+            System.out.println("No matches found.");
+        }
+    }
+
+    /*
+     * Generates a random integer with a maximum size dictated by the number parameter
+     */
+    private static int randomNumber(int number) {
+        return (int)(Math.random() * number) + 1;
     }
     
 }
