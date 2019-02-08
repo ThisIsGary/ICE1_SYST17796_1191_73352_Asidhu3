6d652edfbf5c54d79a7d060f6ff4e9c91360cd92
alasalio <alasalio@shernet.sheridancollege.ca>
2019-02-07 22:11:39 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -5,38 +5,41 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
-/**
- * A class that models playing card Objects. Cards have 
+/*
+ * A class that models playing card Objects. Cards have
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * 
+ *
  * @author
  */
 public class Card {
-    
-    private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
 
+    private String suit; //clubs, spades, diamonds, hearts
+   private int value; //1-13
+   private int obada;
+
+   public Card(int value, String suit){
+    setValue(value);
+    setSuit(suit);
+   }
+   public Card(){};
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
+    /* @return the suit
      */
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
+    /* @param suit the suit to set
      */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
-     * @return the value
+    /* @return the value
      */
     public int getValue() {
         return value;
@@ -48,6 +51,11 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-    
+
+    public String toString(){
+     return value + " " + suit;
+    }
+
+
+
 }
--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,32 +1,63 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Name: Obada Alasali
+Student ID: 991404478
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
- */
+ * @author
+  */
+
 public class CardTrick {
-    
+
     public static void main(String[] args)
     {
+
+     Scanner input = new Scanner(System.in);
         Card[] magicHand = new Card[7];
-        
+        Random rnd = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(rnd.nextInt(13) + 1));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[rnd.nextInt(3) + 1]);
+            int number = c.getValue();
+            String suit = c.getSuit();
+
+            magicHand[i] = new Card(number,suit);
+           //  System.out.println(magicHand[i]); <-- print statement for testing purposes
+
         }
-        
+
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("pick any card");
+        System.out.println("Hearts, Diamonds, Spades, Clubs");
+        String userSuit = input.nextLine();
+        System.out.println("pick a value 1-13");
+        int userValue = input.nextInt();
         // and search magicHand here
-        //Then report the result here
+        for (int i=0; i<magicHand.length; i++){
+         if(magicHand[i].getSuit().equalsIgnoreCase(userSuit) && magicHand[i].getValue() == userValue){
+          System.out.println(" You Guessed the Card right"); //Then report the result here
+         break;
+         }
+
+         else if(i == magicHand.length - 1 && (!magicHand[i].getSuit().equalsIgnoreCase(userSuit) || magicHand[i].getValue() != userValue)){
+          System.out.println("You Guessed wrong"); //Then report the result here
+         }
+
+
+
+
+        }
     }
-    
+
+
 }
