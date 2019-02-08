9329db6c71fdb2f879d370f5c22daa0b0b022b6e
dhillrim <dhillrim@sheridancollege.ca>
2019-02-07 20:25:06 -0500

    eighth commit

--- ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ ICE1_SYST17796_1191_73352_Asidhu3/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Rimpaldeep Kaur Dhillon
+ * 991528804
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
@@ -11,22 +10,56 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author 
  */
+
+import java.util.Scanner;
 public class CardTrick {
-    
+    static int no;
+    static int a;
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        int cardno[]=new int[7];
+        String suit[]=new String[7];
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            no=(int)(Math.random()*13+1);
+            c.setValue(no);
+            cardno[i]=c.getValue();
+            a=(int)Math.random()*3;
+            c.setSuit(Card.SUITS[a]);
+            suit[i]=c.getSuit();
+            System.out.println("Card number is :"+cardno[i]+" and suit is"+suit[i]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        Scanner k=new Scanner(System.in);
+        System.out.println("Enter card value");
+        int value=k.nextInt();
+        System.out.println("Enter suit");
+        String Suit=k.nextLine();
+        
+        for(int i=0;i<cardno.length;i++){
+            if(value==cardno[i])
+                System.out.println("Card no  found in magichand");
+            else
+                System.out.println("Card no not found ");
+        }
+         
+        for(int i=0;i<suit.length;i++){
+            if(Suit.equals(suit[i]))
+                System.out.println("Suit found");
+            else
+                System.out.println("Suit not found");
+        }
+       
+        
+        
+    }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
     }
     
-}
+
